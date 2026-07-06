945e49a9d801ccf522eb19d6d63662d020a50fb8
ioshi9212-dotcom <ioshi9212@gmail.com>
2026-07-06 22:26:29 +1000

    Remove pages cleanup stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import './styles.css';
 import './compact.css';
 import './canvas-scale-fix.css';
 import './button-cleanup.css';
-import './page-block-cleanup.css';
 import './inspector-cleanup.css';
 import App from './AppLive.jsx';
 
