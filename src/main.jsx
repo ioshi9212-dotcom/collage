60f27a4a9e17e8d019989aef3496b007a8bd228b
ioshi9212-dotcom <ioshi9212@gmail.com>
2026-07-07 12:48:56 +1000

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
-import './compact.css';
-import './canvas-scale-fix.css';
-import './button-cleanup.css';
-import './page-block-cleanup.css';
-import './inspector-cleanup.css';
-import './toggle-cleanup.css';
-import './theme-eucalyptus.css';
 import App from './AppLive.jsx';
 
 createRoot(document.getElementById('root')).render(
