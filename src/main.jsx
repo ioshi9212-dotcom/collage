743e550adf1276082379eac609f0ff8081f7a6df
ioshi9212-dotcom <ioshi9212@gmail.com>
2026-07-09 23:39:03 +1000

    Use one stylesheet for editor layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
-import './topbar.css';
 import App from './AppLive.jsx';
 
 createRoot(document.getElementById('root')).render(
