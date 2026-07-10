91ffb65c1c4242424717b04fd78571f9c839e538
ioshi9212-dotcom <ioshi9212@gmail.com>
2026-07-10 13:39:04 +1000

    Load page rail ribbon CSS after main styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles.css';
+import './page-rail-ribbon.css';
 import App from './AppLive.jsx';
 
 createRoot(document.getElementById('root')).render(
