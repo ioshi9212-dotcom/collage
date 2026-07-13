44ba8fe9c4db7eb303b02e507b9d00d0f9b063db
ioshi9212-dotcom <ioshi9212@gmail.com>
2026-07-13 15:53:09 +1000

    Restore image cache test after CI trigger

--- src/editor/imageCache.test.mjs
+++ src/editor/imageCache.test.mjs
@@ -91,4 +91,3 @@ assert.doesNotMatch(appSource, /const imageCache = new Map\(\)/, 'unbounded cach
 assert.doesNotMatch(appSource, /function loadImage\(src\)/, 'AppLive must use the tested loader module');
 
 console.log('bounded image cache checks passed');
-// final CI trigger
